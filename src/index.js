d01a0caa655a16f6ca096418195d983d57775aa1
Aleksandr Zimin <uzao-rec@yandex.run>
2026-04-20 11:23:49 +0300

    refactor: merge readFile+parse+getFormat into single readFile in index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,3 @@
-// import fs from 'fs'
-// import path from 'path'
-// import yaml from 'js-yaml'
-// import buildDiff from './buildDiff.js'
-// import format from './formatters/index.js'
-
-// const readFile = (filepath) => {
-//   const absolutePath = path.resolve(process.cwd(), filepath)
-//   return fs.readFileSync(absolutePath, 'utf-8')
-// }
-
-// const getInputFormat = (filepath) => path.extname(filepath).slice(1)
-
-// const parse = (content, inputFormat) => {
-//   switch (inputFormat) {
-//     case 'json': return JSON.parse(content)
-//     case 'yml':
-//     case 'yaml': return yaml.load(content)
-//     default: throw new Error(`Unknown input format: '${inputFormat}'`)
-//   }
-// }
-
-// const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
-//   const data1 = parse(readFile(filepath1), getInputFormat(filepath1))
-//   const data2 = parse(readFile(filepath2), getInputFormat(filepath2))
-//   const diff = buildDiff(data1, data2)
-//   return format(diff, outputFormat)
-// }
-
-// export default genDiff
-
-// src/index.js — итоговый вид
-
 import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
